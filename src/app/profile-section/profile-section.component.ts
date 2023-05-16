85055a75642b8281363935f8fd29dbca47cfe2af
GuruPrasath6 <gprgp006@gmail.com>
2023-05-16 21:28:38 +0530

    swipe function implemented

--- src/app/profile-section/profile-section.component.ts
+++ src/app/profile-section/profile-section.component.ts
@@ -13,6 +13,10 @@ export class ProfileSectionComponent implements OnInit {
 
   public id: number | undefined;
 
+  private swipeCoord: [number, number] | undefined;
+  private swipeTime: number | undefined;
+
+
   constructor() {
   }
 
@@ -32,5 +36,27 @@ export class ProfileSectionComponent implements OnInit {
     }
   }
 
+
+  /**
+  * Method to handle the swipe guestures
+  */
+  swipe(e: TouchEvent, when: string, id: number): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end' && this.swipeCoord) {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime!;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+        direction[0] < 0 ? this.filter(id, 'shortlist') : this.filter(id, 'reject');
+      }
+    }
+  }
 }
 
